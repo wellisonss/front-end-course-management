77ba2d4802c84b5c49d8ed6744721d2d86c77803
Wellison Santos <silva.s.wellison@gmail.com>
2023-06-10 19:01:28 -0300

    modificações do pcr Marquin em AlunoProvier

--- src/providers/AlunoProvider.ts
+++ src/providers/AlunoProvider.ts
@@ -2,17 +2,12 @@ import { api } from "./api";
 import { IAluno } from "@/Interfaces/IAluno"
 
 const getAlunoApi = async (): Promise<IAluno[]> => {
-  try {
     const response = await api.get("/aluno");
     return response.data;
-  } catch (error) {
-      throw error; 
-  }
 };
 
 const postAlunoApi = async (aluno: IAluno): Promise<IAluno> => {
 
-  try {
     const response = await api.post("/aluno", {
       NOME: aluno.NOME,
       SENHA: aluno.SENHA,
@@ -21,30 +16,21 @@ const postAlunoApi = async (aluno: IAluno): Promise<IAluno> => {
       EMAIL: aluno.EMAIL,
     });
     return response.data;
-  } catch (error) {
-    throw error;
-  }
+
 };
 
 const deleteAlunoApi = async (id: string): Promise<void> => {
-  try {
     await api.delete(`/aluno/${id}`);
-  } catch (error) {
-    throw error;
-  }
 };
 
 const updateAlunoApi = async (aluno: IAluno): Promise<void> => {
-  try {
     await api.put(`/aluno/${aluno.ID}`, {
       NOME: aluno.NOME,
       CURSO: aluno.CURSO,
       EMAIL: aluno.EMAIL,
       SENHA: aluno.SENHA
     });
-  } catch (error) {
-     throw error;
-  }
+
 };
 
 export { getAlunoApi, postAlunoApi, deleteAlunoApi, updateAlunoApi };
